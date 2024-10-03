80d67f0d41dc5dc64cd4825b4c2e1048f3e29fef
garrettbear <gwhisten@gmail.com>
2024-10-02 17:44:28 -0700

    feat: add accessibility tool:

--- docs/storybook-react/.storybook/main.ts
+++ docs/storybook-react/.storybook/main.ts
@@ -4,8 +4,9 @@ import path, { join, dirname } from 'path';
 
 /**
  * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ * It is needed in projects that are set up within a monorepo.
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, 'package.json')));
 }
@@ -19,6 +20,7 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-a11y'),
   ],
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
