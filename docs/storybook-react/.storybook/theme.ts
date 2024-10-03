5613edfe4d26f74c06f7ec2106c83dc51d087bc6
garrettbear <gwhisten@gmail.com>
2024-10-02 21:36:55 -0700

    theme fix

--- docs/storybook-react/.storybook/theme.ts
+++ docs/storybook-react/.storybook/theme.ts
@@ -6,6 +6,5 @@ export default create({
   brandUrl: 'https://metamask.io/',
   brandImage:
     'https://images.ctfassets.net/9sy2a0egs6zh/4zJfzJbG3kTDSk5Wo4RJI1/1b363263141cf629b28155e2625b56c9/mm-logo.svg',
-  brandTarget: '_self',
-  colorPrimary: 'red',
+  brandTarget: '_blank',
 });
