6cc30dfa8b09ab95a74f907370b6b5347cd0437d
Brian Nguyen <brianacnguyen@gmail.com>
2024-10-16 14:26:13 -0700

    chore: initialized working rn

--- packages/design-system-react-native/metro.config.js
+++ packages/design-system-react-native/metro.config.js
@@ -1,21 +1,21 @@
-const exclusionList = require('metro-config/src/defaults/exclusionList');
+/* eslint-disable import/unambiguous */
+// eslint-disable-next-line import/no-unresolved
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const path = require('path');
 
-module.exports = {
-  projectRoot: path.resolve(__dirname),
-  watchFolders: [path.resolve(__dirname, '../../node_modules')],
-  resolver: {
-    blacklistRE: exclusionList([
-      // Avoid conflicts with duplicate modules
-      new RegExp(
-        `${path.resolve(__dirname, '../../node_modules/react-native')}.*`,
-      ),
-    ]),
-    extraNodeModules: new Proxy(
-      {},
-      {
-        get: (target, name) => path.resolve(__dirname, `node_modules/${name}`),
-      },
-    ),
-  },
+const projectRoot = __dirname;
+const monorepoRoot = path.resolve(__dirname, '../../');
+
+const defaultConfig = getDefaultConfig(projectRoot);
+
+const config = {
+  // Project root is the package's directory
+  projectRoot,
+
+  // Watch the monorepo root for changes in shared code
+  watchFolders: [monorepoRoot],
+
+  // Other configurations can be added here
 };
+
+module.exports = mergeConfig(defaultConfig, config);
--- packages/design-system-react-native/react-native.config.js
+++ packages/design-system-react-native/react-native.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/unambiguous */
 module.exports = {
   project: {
     ios: {},
