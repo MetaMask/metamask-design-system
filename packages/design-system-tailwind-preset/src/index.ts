2ad1b78782eabc847d6e4e42ddf9ce5e13ce6b0d
georgewrmarshall <george.marshall@consensys.net>
2024-10-16 20:51:40 -0700

    chore: adding text and background shortcuts

--- packages/design-system-tailwind-preset/src/index.ts
+++ packages/design-system-tailwind-preset/src/index.ts
@@ -7,6 +7,14 @@ const tailwindConfig: Config = {
   theme: {
     extend: {
       ...colors,
+      // Reduces redundancy by enabling shorter Tailwind class names, such as `text-default` instead of `text-text-default`
+      textColor: {
+        ...colors.text,
+      },
+      // Reduces redundancy by enabling shorter Tailwind class names, such as `bg-default` instead of `bg-background-default`
+      backgroundColor: {
+        ...colors.background,
+      },
     },
   },
   plugins: [],
