0f6d3ee0e983b534de8dfd19394253c0dbf94790
georgewrmarshall <george.marshall@consensys.net>
2024-10-16 21:35:19 -0700

    chore: allowing custom colors like text-primary-default to still be available

--- packages/design-system-tailwind-preset/src/index.ts
+++ packages/design-system-tailwind-preset/src/index.ts
@@ -6,15 +6,22 @@ const tailwindConfig: Config = {
   content: [],
   theme: {
     extend: {
-      ...colors,
-      // Reduces redundancy by enabling shorter Tailwind class names, such as `text-default` instead of `text-text-default`
-      textColor: {
-        ...colors.text,
+      colors: {
+        ...colors,
       },
-      // Reduces redundancy by enabling shorter Tailwind class names, such as `bg-default` instead of `bg-background-default`
-      backgroundColor: {
-        ...colors.background,
-      },
+      // Reduces redundancy by enabling shorter Tailwind class names
+      textColor: ({ theme }) => ({
+        ...theme('colors'), // Incorporate existing color utilities like text-primary-default
+        ...colors.text, // e.g. text-default instead of text-text-default
+      }),
+      backgroundColor: ({ theme }) => ({
+        ...theme('colors'), // Incorporate existing color utilities like bg-primary-default
+        ...colors.background, // e.g. bg-default instead of bg-background-default
+      }),
+      borderColor: ({ theme }) => ({
+        ...theme('colors'), // Incorporate existing color utilities like border-primary-default
+        ...colors.border, // e.g. border-default instead of border-border-default
+      }),
     },
   },
   plugins: [],
