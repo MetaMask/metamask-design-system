c5045749374fa44f1c502e239c01b24af484b129
georgewrmarshall <george.marshall@consensys.net>
2024-10-22 15:02:12 -0700

    chore: removing unecessary files and scripts

--- apps/storybook-react-native/.eslintrc.js
+++ apps/storybook-react-native/.eslintrc.js
@@ -6,16 +6,6 @@ module.exports = {
   ],
   overrides: [
     {
-      files: ['*.js', '*.jsx'],
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module', // Allows ES6 import/export syntax
-      },
-      rules: {
-        // Add any package-specific rules here
-      },
-    },
-    {
       files: ['*.ts', '*.tsx'],
       extends: ['@metamask/eslint-config-typescript'],
       parser: '@typescript-eslint/parser',
