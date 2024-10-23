96dc509d6a97cd62df4940d82a6735e2879d8c0a
georgewrmarshall <george.marshall@consensys.net>
2024-10-23 14:36:28 -0700

    chore: fixing eslint

--- apps/storybook-react/tailwind.config.js
+++ apps/storybook-react/tailwind.config.js
@@ -1,10 +1,11 @@
+// eslint-disable-next-line global-require
 const designSystemPreset = require('@metamask/design-system-tailwind-preset');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [designSystemPreset],
   content: [
-    // '../../packages/react-components/src/**/*.{js,jsx,ts,tsx}', // TO DO: Update when react component package is created
+    // '../../packages/react-components/src/**/*.{js,jsx,ts,tsx}', TO DO: Update when react component package is created
     './src/**/*.{js,jsx,ts,tsx}',
     './stories/**/*.{js,jsx,ts,tsx}',
   ],
