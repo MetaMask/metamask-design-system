95a6d2b60f00cd8d86b804ae9f18ae20de5d0818
georgewrmarshall <george.marshall@consensys.net>
2024-10-24 15:35:04 -0700

    chore: adding tests for shadow preset and plugin

--- packages/design-system-tailwind-preset/src/shadows.ts
+++ packages/design-system-tailwind-preset/src/shadows.ts
@@ -1,3 +1,5 @@
+import plugin from 'tailwindcss/plugin';
+
 export const shadows = {
   xs: 'var(--shadow-size-xs) var(--shadow-color, var(--color-shadow-default))',
   sm: 'var(--shadow-size-sm) var(--shadow-color, var(--color-shadow-default))',
@@ -10,3 +12,30 @@ export const shadowColors = {
   primary: 'var(--color-shadow-primary)',
   error: 'var(--color-shadow-error)',
 };
+
+// Define and export the shadow plugin
+export const shadowPlugin = plugin(function ({
+  addUtilities,
+}: {
+  addUtilities: (
+    utilities: Record<string, Record<string, string>>,
+    options?: Partial<{
+      respectPrefix: boolean;
+      respectImportant: boolean;
+    }>,
+  ) => void;
+}) {
+  const shadowColorUtilities: Record<string, Record<string, string>> = {};
+
+  Object.entries(shadowColors).forEach(([key, value]) => {
+    shadowColorUtilities[`.shadow-${key}`] = {
+      '--shadow-color': value, // This ensures that --shadow-color is set to the correct color value
+    };
+  });
+
+  // Add the utilities with Tailwind’s addUtilities method
+  addUtilities(shadowColorUtilities, {
+    respectPrefix: false,
+    respectImportant: true,
+  });
+});
