66c20d1a302bac5ee6a304d68f16ce730a0a75ec
georgewrmarshall <george.marshall@consensys.net>
2024-11-14 17:01:47 -0800

    chore: storybook clean up

--- apps/storybook-react/vite.config.ts
+++ apps/storybook-react/vite.config.ts
@@ -1,14 +1,5 @@
-// import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  resolve: {
-    // TO DO: Update when react component package is created
-    // alias: {
-    //   '@components': path.resolve(
-    //     __dirname,
-    //     '../packages/react-components/src',
-    //   ),
-    // },
-  },
+  resolve: {},
 });
