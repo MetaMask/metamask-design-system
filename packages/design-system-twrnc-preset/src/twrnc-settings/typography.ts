58f55399f629ffde42bb18e1296cbecc3f826e65
Brian Nguyen <brianacnguyen@gmail.com>
2024-11-25 08:15:56 -0800

    Fixed lint errors

--- packages/design-system-twrnc-preset/src/twrnc-settings/typography.ts
+++ packages/design-system-twrnc-preset/src/twrnc-settings/typography.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
 import { typography } from '@metamask/design-tokens';
 
 import type {
@@ -12,64 +13,64 @@ export const typographyTailwindConfig: TypographyTailwindConfigProps = {
     'display-md': [
       typography.sDisplayMD.fontSize,
       {
-        lineHeight: `${typography.sDisplayMD.lineHeight}px`,
-        letterSpacing: `${typography.sDisplayMD.letterSpacing}`,
+        lineHeight: `${typography.sDisplayMD.lineHeight as number}px`,
+        letterSpacing: `${typography.sDisplayMD.letterSpacing as number}`,
         fontWeight: typography.sDisplayMD.fontWeight,
       },
     ],
     'heading-lg': [
       typography.sHeadingLG.fontSize,
       {
-        lineHeight: `${typography.sHeadingLG.lineHeight}px`,
-        letterSpacing: `${typography.sHeadingLG.letterSpacing}`,
+        lineHeight: `${typography.sHeadingLG.lineHeight as number}px`,
+        letterSpacing: `${typography.sHeadingLG.letterSpacing as number}`,
         fontWeight: typography.sHeadingLG.fontWeight,
       },
     ],
     'heading-md': [
       typography.sHeadingMD.fontSize,
       {
-        lineHeight: `${typography.sHeadingMD.lineHeight}px`,
-        letterSpacing: `${typography.sHeadingMD.letterSpacing}`,
+        lineHeight: `${typography.sHeadingMD.lineHeight as number}px`,
+        letterSpacing: `${typography.sHeadingMD.letterSpacing as number}`,
         fontWeight: typography.sHeadingMD.fontWeight,
       },
     ],
     'heading-sm': [
       typography.sHeadingSM.fontSize,
       {
-        lineHeight: `${typography.sHeadingSM.lineHeight}px`,
-        letterSpacing: `${typography.sHeadingSM.letterSpacing}`,
+        lineHeight: `${typography.sHeadingSM.lineHeight as number}px`,
+        letterSpacing: `${typography.sHeadingSM.letterSpacing as number}`,
         fontWeight: typography.sHeadingSM.fontWeight,
       },
     ],
     'body-lg': [
       typography.sBodyLGMedium.fontSize,
       {
-        lineHeight: `${typography.sBodyLGMedium.lineHeight}px`,
-        letterSpacing: `${typography.sBodyLGMedium.letterSpacing}`,
+        lineHeight: `${typography.sBodyLGMedium.lineHeight as number}px`,
+        letterSpacing: `${typography.sBodyLGMedium.letterSpacing as number}`,
         fontWeight: typography.sBodyLGMedium.fontWeight,
       },
     ],
     'body-md': [
       typography.sBodyMD.fontSize,
       {
-        lineHeight: `${typography.sBodyMD.lineHeight}px`,
-        letterSpacing: `${typography.sBodyMD.letterSpacing}`,
+        lineHeight: `${typography.sBodyMD.lineHeight as number}px`,
+        letterSpacing: `${typography.sBodyMD.letterSpacing as number}`,
         fontWeight: typography.sBodyMD.fontWeight,
       },
     ],
     'body-sm': [
       typography.sBodySM.fontSize,
       {
-        lineHeight: `${typography.sBodySM.lineHeight}px`,
-        letterSpacing: `${typography.sBodySM.letterSpacing}`,
+        lineHeight: `${typography.sBodySM.lineHeight as number}px`,
+        letterSpacing: `${typography.sBodySM.letterSpacing as number}`,
         fontWeight: typography.sBodySM.fontWeight,
       },
     ],
     'body-xs': [
       typography.sBodyXS.fontSize,
       {
-        lineHeight: `${typography.sBodyXS.lineHeight}px`,
-        letterSpacing: `${typography.sBodyXS.letterSpacing}`,
+        lineHeight: `${typography.sBodyXS.lineHeight as number}px`,
+        letterSpacing: `${typography.sBodyXS.letterSpacing as number}`,
         fontWeight: typography.sBodyXS.fontWeight,
       },
     ],
@@ -215,23 +216,23 @@ export const typographyTailwindConfig: TypographyTailwindConfigProps = {
     ),
   },
   letterSpacing: {
-    'display-md': `${typography.sDisplayMD.letterSpacing}`,
-    'heading-lg': `${typography.sHeadingLG.letterSpacing}`,
-    'heading-md': `${typography.sHeadingMD.letterSpacing}`,
-    'heading-sm': `${typography.sHeadingSM.letterSpacing}`,
-    'body-lg': `${typography.sBodyLGMedium.letterSpacing}`,
-    'body-md': `${typography.sBodyMD.letterSpacing}`,
-    'body-sm': `${typography.sBodySM.letterSpacing}`,
-    'body-xs': `${typography.sBodyXS.letterSpacing}`,
+    'display-md': `${typography.sDisplayMD.letterSpacing as number}`,
+    'heading-lg': `${typography.sHeadingLG.letterSpacing as number}`,
+    'heading-md': `${typography.sHeadingMD.letterSpacing as number}`,
+    'heading-sm': `${typography.sHeadingSM.letterSpacing as number}`,
+    'body-lg': `${typography.sBodyLGMedium.letterSpacing as number}`,
+    'body-md': `${typography.sBodyMD.letterSpacing as number}`,
+    'body-sm': `${typography.sBodySM.letterSpacing as number}`,
+    'body-xs': `${typography.sBodyXS.letterSpacing as number}`,
   },
   lineHeight: {
-    'display-md': `${typography.sDisplayMD.lineHeight}px`,
-    'heading-lg': `${typography.sHeadingLG.lineHeight}px`,
-    'heading-md': `${typography.sHeadingMD.lineHeight}px`,
-    'heading-sm': `${typography.sHeadingSM.lineHeight}px`,
-    'body-lg': `${typography.sBodyLGMedium.lineHeight}px`,
-    'body-md': `${typography.sBodyMD.lineHeight}px`,
-    'body-sm': `${typography.sBodySM.lineHeight}px`,
-    'body-xs': `${typography.sBodyXS.lineHeight}px`,
+    'display-md': `${typography.sDisplayMD.lineHeight as number}px`,
+    'heading-lg': `${typography.sHeadingLG.lineHeight as number}px`,
+    'heading-md': `${typography.sHeadingMD.lineHeight as number}px`,
+    'heading-sm': `${typography.sHeadingSM.lineHeight as number}px`,
+    'body-lg': `${typography.sBodyLGMedium.lineHeight as number}px`,
+    'body-md': `${typography.sBodyMD.lineHeight as number}px`,
+    'body-sm': `${typography.sBodySM.lineHeight as number}px`,
+    'body-xs': `${typography.sBodyXS.lineHeight as number}px`,
   },
 };
