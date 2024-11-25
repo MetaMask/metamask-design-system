91fc99e1d401e7c9ceca644f8889edeefabf720f
Brian Nguyen <brianacnguyen@gmail.com>
2024-11-25 15:04:07 -0800

    Added ColorScheme enum

--- packages/design-system-twrnc-preset/src/Theme/Theme.types.ts
+++ packages/design-system-twrnc-preset/src/Theme/Theme.types.ts
@@ -1,11 +1,12 @@
 import type { create } from 'twrnc';
 
+import { ColorScheme } from '../twrnc-settings';
 import type { ColorSet } from '../twrnc-settings';
 
 export enum Theme {
   Default = 'default',
-  Light = 'light',
-  Dark = 'dark',
+  Light = ColorScheme.Light,
+  Dark = ColorScheme.Dark,
 }
 
 export type ThemeContextProps = {
