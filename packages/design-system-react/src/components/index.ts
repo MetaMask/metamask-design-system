d6dfa8b9baf3a63e4a1811db190ed2ef2466b99a
georgewrmarshall <george.marshall@consensys.net>
2024-12-10 17:03:46 +1300

    chore: adding props and documentation aligned with insight report

--- packages/design-system-react/src/components/index.ts
+++ packages/design-system-react/src/components/index.ts
@@ -14,5 +14,6 @@ export { Icon } from './icon';
 export { IconName, IconSize, IconColor } from './icon';
 export type { IconProps } from './icon';
 
-export { ButtonBase } from './buttonbase';
-export type { ButtonBaseProps } from './buttonbase';
+export { ButtonBase } from './button-base';
+export type { ButtonBaseProps } from './button-base';
+export { ButtonBaseSize } from './button-base';
