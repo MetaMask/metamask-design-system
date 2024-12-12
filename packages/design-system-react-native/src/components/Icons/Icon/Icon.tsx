fe0c9bcde9024600350be8e3e92af036462c2ca1
Brian Nguyen <brianacnguyen@gmail.com>
2024-12-11 17:08:45 -0800

    Add memo to icons

--- packages/design-system-react-native/src/components/Icons/Icon/Icon.tsx
+++ packages/design-system-react-native/src/components/Icons/Icon/Icon.tsx
@@ -2,7 +2,7 @@ import {
   useTailwind,
   withThemeProvider,
 } from '@metamask/design-system-twrnc-preset';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { assetByIconName } from './Icon.assets';
 import { DEFAULT_ICON_SIZE, DEFAULT_ICON_COLOR } from './Icon.constants';
@@ -18,16 +18,12 @@ const Icon = ({
 }: IconProps) => {
   const tw = useTailwind();
   const SVG = assetByIconName[name];
-  const mergedClassnames = generateIconClassNames({ color, size });
+  const styles = useMemo(() => {
+    const mergedClassnames = generateIconClassNames({ color, size });
+    return tw`${mergedClassnames}`;
+  }, [color, size, tw]);
 
-  return (
-    <SVG
-      name={name}
-      fill="currentColor"
-      style={[tw`${mergedClassnames}`]}
-      {...props}
-    />
-  );
+  return <SVG name={name} fill="currentColor" style={[styles]} {...props} />;
 };
 
 export default withThemeProvider(Icon);
