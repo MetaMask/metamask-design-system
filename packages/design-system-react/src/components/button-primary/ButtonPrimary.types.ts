f508f344909bff28495ca6a7489f20c0afeb4ee7
georgewrmarshall <george.marshall@consensys.net>
2024-12-13 12:24:22 +1300

    chore: adding types and docs

--- packages/design-system-react/src/components/button-primary/ButtonPrimary.types.ts
+++ packages/design-system-react/src/components/button-primary/ButtonPrimary.types.ts
@@ -2,7 +2,8 @@ import type { ButtonBaseProps } from '../button-base';
 
 export type ButtonPrimaryProps = Omit<
   ButtonBaseProps,
-  'className' // We want to handle className ourselves
+  // We handle these props in ButtonPrimary
+  'className' | 'isDisabled' | 'isLoading' | 'style'
 > & {
   /**
    * Optional prop for additional CSS classes to be applied to the ButtonPrimary component
@@ -18,4 +19,19 @@ export type ButtonPrimaryProps = Omit<
    * @default false
    */
   isInverse?: boolean;
+  /**
+   * Optional prop that when true, disables the button
+   * @default false
+   */
+  isDisabled?: boolean;
+  /**
+   * Optional prop that when true, shows a loading spinner
+   * @default false
+   */
+  isLoading?: boolean;
+  /**
+   * Optional CSS styles to be applied to the component.
+   * Should be used sparingly and only for dynamic styles that can't be achieved with className.
+   */
+  style?: React.CSSProperties;
 };
