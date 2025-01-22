8dff1c4b2d3466ef1c8226b5c33a82998c61feae
georgewrmarshall <george.marshall@consensys.net>
2025-01-21 16:04:14 -0800

    chore: exporting types from index in js tokens

--- packages/design-tokens/src/index.ts
+++ packages/design-tokens/src/index.ts
@@ -1,2 +1,4 @@
-export type { Theme } from './js';
 export { colors, typography, lightTheme, darkTheme, brandColor } from './js';
+export type { Theme, ThemeColors, ThemeShadows } from './js';
+export type { ThemeTypography } from './js/typography';
+export type { BrandColor } from './js/brandColor/brandColor.types';
--- packages/design-tokens/src/js/index.ts
+++ packages/design-tokens/src/js/index.ts
@@ -1,4 +1,3 @@
-export type { Theme } from './themes';
 export { lightTheme } from './themes';
 export { darkTheme } from './themes';
 export { brandColor } from './brandColor';
@@ -6,3 +5,7 @@ export { brandColor } from './brandColor';
 // DEPRECATED in favor of importing theme objects above
 export { colors } from './colors';
 export { typography } from './typography';
+
+export type { Theme, ThemeColors, ThemeShadows } from './themes/types';
+export type { ThemeTypography } from './typography';
+export type { BrandColor } from './brandColor/brandColor.types';
