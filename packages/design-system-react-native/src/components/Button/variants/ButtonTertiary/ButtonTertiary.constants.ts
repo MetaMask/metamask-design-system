4290570e8c924e8f3d53e80ccb8b9b8a2176d2e3
Brian Nguyen <brianacnguyen@gmail.com>
2025-02-03 19:34:04 -0800

    Add ButtonTertiary to dsrn

---
+++ packages/design-system-react-native/src/components/Button/variants/ButtonTertiary/ButtonTertiary.constants.ts
@@ -0,0 +1,25 @@
+import { DEFAULT_BUTTONBASE_PROPS } from '../../../../primitives/ButtonBase/ButtonBase.constants';
+import { IconColor } from '../../../Icon';
+import { TextColor } from '../../../Text';
+import type { ButtonTertiaryProps } from './ButtonTertiary.types';
+
+// Defaults
+export const DEFAULT_BUTTONTERTIARY_PROPS: Partial<ButtonTertiaryProps> = {
+  ...DEFAULT_BUTTONBASE_PROPS,
+  textProps: {
+    ...DEFAULT_BUTTONBASE_PROPS.textProps,
+    color: TextColor.PrimaryInverse,
+  },
+  spinnerProps: {
+    ...DEFAULT_BUTTONBASE_PROPS.spinnerProps,
+    color: IconColor.PrimaryInverse,
+  },
+  startIconProps: {
+    ...DEFAULT_BUTTONBASE_PROPS.startIconProps,
+    color: IconColor.PrimaryInverse,
+  },
+  endIconProps: {
+    ...DEFAULT_BUTTONBASE_PROPS.endIconProps,
+    color: IconColor.PrimaryInverse,
+  },
+};
