392ae2de99f39e2b30cffb693847e2cb59905857
Brian August Nguyen <brianacnguyen@gmail.com>
2025-02-06 09:54:17 -0800

    Update packages/design-system-react-native/src/components/Button/Button.tsx
    Co-authored-by: George Marshall <georgewrmarshall@gmail.com>

--- packages/design-system-react-native/src/components/Button/Button.tsx
+++ packages/design-system-react-native/src/components/Button/Button.tsx
@@ -17,7 +17,7 @@ const Button = (buttonProps: ButtonProps) => {
     case ButtonVariant.Secondary:
       return <ButtonSecondary {...restProps} />;
     default:
-      throw new Error('Invalid Button Variant');
+      throw new Error(`Invalid Button Variant: ${variant}. Expected one of: ${Object.values(ButtonVariant).join(', ')}`);
   }
 };
 
