daf6c01cc8f18945d002d4294097e19b7e150c4b
Brian Nguyen <brianacnguyen@gmail.com>
2025-03-10 13:56:38 -0700

    Initial badgestatus

---
+++ packages/design-system-react-native/src/components/BadgeStatus/BadgeStatus.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+import { useTailwind } from '@metamask/design-system-twrnc-preset';
+import React from 'react';
+import { View } from 'react-native';
+
+import {
+  DEFAULT_BADGESTATUS_PROPS,
+  TWCLASSMAP_BADGESTATUS_STATUS_BACKGROUNDCOLOR,
+  TWCLASSMAP_BADGESTATUS_STATUS_INNER_BORDERCOLOR,
+} from './BadgeStatus.constants';
+import type { BadgeStatusProps } from './BadgeStatus.types';
+import AvatarBase from '../../primitives/AvatarBase';
+
+const BadgeStatus = ({
+  status,
+  size = DEFAULT_BADGESTATUS_PROPS.size,
+  hasBorder = DEFAULT_BADGESTATUS_PROPS.hasBorder,
+  twClassName = '',
+  style,
+  ...props
+}: BadgeStatusProps) => {
+  const tw = useTailwind();
+
+  return (
+    <View
+      style={[
+        tw`
+        self-start
+        rounded-full 
+        ${hasBorder ? 'border-[2px] border-background-default' : ''} 
+        ${twClassName}`,
+      ]}
+      {...props}
+    >
+      <View
+        style={[
+          tw`
+            h-[${size}px] 
+            w-[${size}px] 
+            ${TWCLASSMAP_BADGESTATUS_STATUS_BACKGROUNDCOLOR[status]}
+            rounded-full 
+            border-[2px]
+            ${TWCLASSMAP_BADGESTATUS_STATUS_INNER_BORDERCOLOR[status]}`,
+        ]}
+      />
+    </View>
+  );
+};
+
+export default BadgeStatus;
---
+++ packages/design-system-react-native/src/components/BadgeStatus/index.ts
@@ -0,0 +1,4 @@
+export { default } from './BadgeStatus';
+export type { AvatarIconProps } from './BadgeStatus.types';
+export { AvatarIconSize } from '../../shared/enums';
+export { AvatarIconSeverity } from './BadgeStatus.types';
