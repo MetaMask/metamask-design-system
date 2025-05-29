86afcdf44666207c95b0a3fbff2385d9950656ec
Brian August Nguyen <brianacnguyen@gmail.com>
2025-05-28 17:20:19 -0700

    [DSR] Added Checkbox (#679)
    <!--
    Please submit this PR as a draft initially.
    Do not mark it as "Ready for review" until the template has been
    completely filled out, and PR status checks have passed at least once.
    -->
    
    ## **Description**
    This PR adds the `Checkbox` component to the
    `@metamask/design-system-react` package
    <!--
    Write a short description of the changes included in this pull request,
    also include relevant motivation and context. Have in mind the following
    questions:
    1. What is the reason for the change?
    2. What is the improvement/solution?
    -->
    
    ## **Related issues**
    
    Fixes: #665 
    
    ## **Manual testing steps**
    
    1. Run `yarn storybook`
    2. Go to React Components > Checkbox
    3.
    
    ## **Screenshots/Recordings**
    
    <!-- If applicable, add screenshots and/or recordings to visualize the
    before and after of your change. -->
    
    ### **Before**
    
    <!-- [screenshots/recordings] -->
    
    ### **After**
    
    
    
    
    https://github.com/user-attachments/assets/0b682f7f-258f-4b0e-bdcf-d8ee4a174dba
    
    
    
    <!-- [screenshots/recordings] -->
    
    ## **Pre-merge author checklist**
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs)
    - [x] I've completed the PR template to the best of my ability
    - [x] I’ve included tests if applicable
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [x] I’ve applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g. pull and build branch, run the
    app, test code being changed).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in the ticket it closes and includes the necessary testing evidence such
    as recordings and or screenshots.
    
    ---------
    
    Co-authored-by: georgewrmarshall <george.marshall@consensys.net>
    Co-authored-by: George Marshall <georgewrmarshall@gmail.com>

---
+++ packages/design-system-react/src/components/Checkbox/Checkbox.types.ts
@@ -0,0 +1,84 @@
+import type { ComponentProps } from 'react';
+
+import type { IconProps } from '../Icon';
+import type { TextProps } from '../Text';
+
+export type CheckboxProps = Omit<
+  ComponentProps<'label'>,
+  'style' | 'className' | 'children' | 'htmlFor'
+> & {
+  /**
+   * Required unique identifier for the checkbox input element.
+   * This is used for the input's ID and the label's htmlFor attributes.
+   */
+  id: string;
+
+  /**
+   * Required prop to determine whether the checkbox is currently selected.
+   * This component is fully controlled, so you must manage this state
+   * in your parent component.
+   */
+  isSelected: boolean;
+
+  /**
+   * Optional prop that when true, disables the checkbox.
+   *
+   * @default false
+   */
+  isDisabled?: boolean;
+
+  /**
+   * Optional prop that when true, displays the invalid/error state of the checkbox.
+   *
+   * @default false
+   */
+  isInvalid?: boolean;
+
+  /**
+   * Optional label prop that renders text or a React node as a label beside the checkbox.
+   */
+  label?: React.ReactNode | string;
+
+  /**
+   * Optional props to be passed to the label's Text component
+   */
+  labelProps?: Omit<Partial<TextProps>, 'children'>;
+
+  /**
+   * Required callback for when the checked state changes.
+   * Use this to update your state.
+   */
+  onChange: (isSelected: boolean) => void;
+
+  /**
+   * Optional props passed to the input element.
+   */
+  inputProps?: Omit<
+    ComponentProps<'input'>,
+    'type' | 'checked' | 'onChange' | 'disabled'
+  > & {
+    [key: `data-${string}`]: string;
+  };
+
+  /**
+   * Optional props passed to the container div wrapping the checkbox icon.
+   */
+  checkboxContainerProps?: (Omit<ComponentProps<'div'>, 'children'> & {
+    className?: string;
+  }) &
+    Record<string, unknown>;
+
+  /**
+   * Optional props to be passed to the check Icon component
+   */
+  checkedIconProps?: Partial<IconProps>;
+  /**
+   * Optional prop for additional CSS classes to be applied to the Checkbox component.
+   * These classes will be merged with the component's default classes using twMerge.
+   */
+  className?: string;
+  /**
+   * Optional CSS styles for the outer container.
+   */
+  style?: React.CSSProperties;
+};
