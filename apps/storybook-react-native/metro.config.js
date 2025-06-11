58f2edc350c2f759a9b76454e13c48b19bcc4a07
georgewrmarshall <george.marshall@consensys.net>
2025-06-11 11:28:44 +0930

    chore: upgrade of react native and storybook

--- apps/storybook-react-native/metro.config.js
+++ apps/storybook-react-native/metro.config.js
@@ -1,16 +1,31 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (async () => {
-  const {
-    resolver: { assetExts, sourceExts },
-  } = await getDefaultConfig(__dirname);
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg', 'mjs'],
-    },
-  };
-})();
+const defaultConfig = getDefaultConfig(__dirname);
+
+// Add SVG transformer support
+const {
+  resolver: { assetExts, sourceExts },
+} = defaultConfig;
+defaultConfig.transformer = {
+  ...defaultConfig.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
+defaultConfig.resolver = {
+  ...defaultConfig.resolver,
+  assetExts: assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...sourceExts, 'svg', 'mjs'],
+  // Prevent multiple React instances
+  alias: {
+    react: path.resolve(__dirname, 'node_modules/react'),
+    'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+  },
+};
+
+const withStorybook = require('@storybook/react-native/metro/withStorybook');
+
+module.exports = withStorybook(defaultConfig, {
+  enabled: process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === 'true',
+  configPath: path.resolve(__dirname, './.rnstorybook'),
+  onDisabledRemoveStorybook: true,
+});
