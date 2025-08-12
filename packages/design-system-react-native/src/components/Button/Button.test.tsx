22f6102ee3d9d98217e0e1a26f682af92b99e0db
georgewrmarshall <george.marshall@consensys.net>
2025-08-12 13:02:20 -0700

    chore: updating tests

--- packages/design-system-react-native/src/components/Button/Button.test.tsx
+++ packages/design-system-react-native/src/components/Button/Button.test.tsx
@@ -6,6 +6,13 @@ import { ButtonVariant } from '../../types';
 import { Button } from './Button';
 
 describe('Button', () => {
+  it('renders primary variant by default when no variant is provided', () => {
+    const { getByTestId } = render(
+      <Button testID="button-default">Default Button</Button>,
+    );
+    expect(getByTestId('button-default')).not.toBeNull();
+  });
+
   it('renders the correct primary variant', () => {
     const { getByTestId } = render(
       <Button variant={ButtonVariant.Primary} testID="button-primary">
