8cb7f1696fe6c57eabb476ed2f901a696420c712
georgewrmarshall <george.marshall@consensys.net>
2025-08-19 21:41:59 -0700

    chore: removing style omit from types

--- packages/design-system-react/src/components/temp-components/Maskicon/Maskicon.types.ts
+++ packages/design-system-react/src/components/temp-components/Maskicon/Maskicon.types.ts
@@ -3,10 +3,7 @@ import type { ComponentProps } from 'react';
 /**
  * Maskicon component props.
  */
-export type MaskiconProps = Omit<
-  ComponentProps<'img'>,
-  'width' | 'height' | 'style'
-> & {
+export type MaskiconProps = Omit<ComponentProps<'img'>, 'width' | 'height'> & {
   /**
    * Required address used as a unique identifier to generate the Maskicon.
    */
