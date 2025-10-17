7346ed00cea3a78320aee135e62fb3d29c898c4d
georgewrmarshall <george.marshall@consensys.net>
2025-10-16 17:32:06 -0700

    chore: update

--- packages/design-system-react/src/components/Text/Text.constants.ts
+++ packages/design-system-react/src/components/Text/Text.constants.ts
@@ -1,34 +1,20 @@
 import { TextVariant } from '../../types';
 
 export const CLASSMAP_TEXT_VARIANT_FONTSTYLE: Record<TextVariant, string> = {
-  [TextVariant.DisplayLg]:
-    'text-s-display-lg leading-s-display-lg tracking-s-display-lg md:text-l-display-lg md:leading-l-display-lg md:tracking-l-display-lg',
-  [TextVariant.DisplayMd]:
-    'text-s-display-md leading-s-display-md tracking-s-display-md md:text-l-display-md md:leading-l-display-md md:tracking-l-display-md',
-  [TextVariant.HeadingLg]:
-    'text-s-heading-lg leading-s-heading-lg tracking-s-heading-lg md:text-l-heading-lg md:leading-l-heading-lg md:tracking-l-heading-lg',
-  [TextVariant.HeadingMd]:
-    'text-s-heading-md leading-s-heading-md tracking-s-heading-md md:text-l-heading-md md:leading-l-heading-md md:tracking-l-heading-md',
-  [TextVariant.HeadingSm]:
-    'text-s-heading-sm leading-s-heading-sm tracking-s-heading-sm md:text-l-heading-sm md:leading-l-heading-sm md:tracking-l-heading-sm',
-  [TextVariant.BodyLg]:
-    'text-s-body-lg leading-s-body-lg tracking-s-body-lg md:text-l-body-lg md:leading-l-body-lg md:tracking-l-body-lg',
-  [TextVariant.BodyMd]:
-    'text-s-body-md leading-s-body-md tracking-s-body-md md:text-l-body-md md:leading-l-body-md md:tracking-l-body-md',
-  [TextVariant.BodySm]:
-    'text-s-body-sm leading-s-body-sm tracking-s-body-sm md:text-l-body-sm md:leading-l-body-sm md:tracking-l-body-sm',
-  [TextVariant.BodyXs]:
-    'text-s-body-xs leading-s-body-xs tracking-s-body-xs md:text-l-body-xs md:leading-l-body-xs md:tracking-l-body-xs',
-  [TextVariant.PageHeading]:
-    'text-s-page-heading leading-s-page-heading tracking-s-page-heading md:text-l-page-heading md:leading-l-page-heading md:tracking-l-page-heading',
-  [TextVariant.SectionHeading]:
-    'text-s-section-heading leading-s-section-heading tracking-s-section-heading md:text-l-section-heading md:leading-l-section-heading md:tracking-l-section-heading',
-  [TextVariant.ButtonLabelMd]:
-    'text-s-button-label-md leading-s-button-label-md tracking-s-button-label-md md:text-l-button-label-md md:leading-l-button-label-md md:tracking-l-button-label-md',
-  [TextVariant.ButtonLabelLg]:
-    'text-s-button-label-lg leading-s-button-label-lg tracking-s-button-label-lg md:text-l-button-label-lg md:leading-l-button-label-lg md:tracking-l-button-label-lg',
-  [TextVariant.AmountDisplayLg]:
-    'text-s-amount-display-lg leading-s-amount-display-lg tracking-s-amount-display-lg md:text-l-amount-display-lg md:leading-l-amount-display-lg md:tracking-l-amount-display-lg',
+  [TextVariant.DisplayLg]: 'text-display-lg',
+  [TextVariant.DisplayMd]: 'text-display-md',
+  [TextVariant.HeadingLg]: 'text-heading-lg',
+  [TextVariant.HeadingMd]: 'text-heading-md',
+  [TextVariant.HeadingSm]: 'text-heading-sm',
+  [TextVariant.BodyLg]: 'text-body-lg',
+  [TextVariant.BodyMd]: 'text-body-md',
+  [TextVariant.BodySm]: 'text-body-sm',
+  [TextVariant.BodyXs]: 'text-body-xs',
+  [TextVariant.PageHeading]: 'text-page-heading',
+  [TextVariant.SectionHeading]: 'text-section-heading',
+  [TextVariant.ButtonLabelMd]: 'text-button-label-md',
+  [TextVariant.ButtonLabelLg]: 'text-button-label-lg',
+  [TextVariant.AmountDisplayLg]: 'text-amount-display-lg',
 };
 
 export const CLASSMAP_TEXT_VARIANT_FONTWEIGHT: Record<TextVariant, string> = {
