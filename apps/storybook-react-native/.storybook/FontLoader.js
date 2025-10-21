a76055237e8fde44f8ac51843246c5563a4752eb
georgewrmarshall <george.marshall@consensys.net>
2025-10-21 09:07:50 -0700

    chore: moving fonts to correct folder

--- apps/storybook-react-native/.storybook/FontLoader.js
+++ apps/storybook-react-native/.storybook/FontLoader.js
@@ -8,16 +8,18 @@ const FontLoader = ({ children }) => {
   useEffect(() => {
     (async () => {
       await Font.loadAsync({
-        'Geist Regular': require('../fonts/Geist/Geist Regular.otf'),
-        'Geist Regular Italic': require('../fonts/Geist/Geist Regular Italic.otf'),
-        'Geist Medium': require('../fonts/Geist/Geist Medium.otf'),
-        'Geist Medium Italic': require('../fonts/Geist/Geist Medium Italic.otf'),
-        'Geist SemiBold': require('../fonts/Geist/Geist SemiBold.otf'),
-        'Geist SemiBold Italic': require('../fonts/Geist/Geist SemiBold Italic.otf'),
-        'MM Poly Regular': require('../fonts/MMPoly/MM Poly Regular.otf'),
-        'MM Sans Regular': require('../fonts/MMSans/MM Sans Regular.otf'),
-        'MM Sans Medium': require('../fonts/MMSans/MM Sans Medium.otf'),
-        'MM Sans Bold': require('../fonts/MMSans/MM Sans Bold.otf'),
+        'Geist Regular': require('../assets/fonts/Geist/Geist Regular.otf'),
+        'Geist Regular Italic': require('../assets/fonts/Geist/Geist Regular Italic.otf'),
+        'Geist Medium': require('../assets/fonts/Geist/Geist Medium.otf'),
+        'Geist Medium Italic': require('../assets/fonts/Geist/Geist Medium Italic.otf'),
+        'Geist SemiBold': require('../assets/fonts/Geist/Geist SemiBold.otf'),
+        'Geist SemiBold Italic': require('../assets/fonts/Geist/Geist SemiBoldItalic.otf'),
+        'Geist Bold': require('../assets/fonts/Geist/Geist Bold.otf'),
+        'Geist Bold Italic': require('../assets/fonts/Geist/Geist Bold Italic.otf'),
+        'MM Poly Regular': require('../assets/fonts/MMPoly/MM Poly Regular.otf'),
+        'MM Sans Regular': require('../assets/fonts/MMSans/MM Sans Regular.otf'),
+        'MM Sans Medium': require('../assets/fonts/MMSans/MM Sans Medium.otf'),
+        'MM Sans Bold': require('../assets/fonts/MMSans/MM Sans Bold.otf'),
       });
       setFontsLoaded(true);
     })();
