7079c079955b25198a0c87ba835a33a1c58f87f8
georgewrmarshall <george.marshall@consensys.net>
2025-11-11 15:25:49 -0800

    chore: making storybook render by default

--- apps/storybook-react-native/App.tsx
+++ apps/storybook-react-native/App.tsx
@@ -1,30 +1,3 @@
-import { StyleSheet, Text, View } from 'react-native';
-
 import StorybookUIRoot from './.rnstorybook';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-/**
- * Default App component
- *
- * @returns React component
- */
-function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
-
-const AppEntryPoint =
-  process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === 'true' ? StorybookUIRoot : App;
-
-export default AppEntryPoint;
+export default StorybookUIRoot;
