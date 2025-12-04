c47bb52216c308ec31e3cfe58c2f9f70b17d43c1
georgewrmarshall <george.marshall@consensys.net>
2025-12-05 09:39:30 +1300

    fix: updating font names to align with mobile and using post sc ript names

--- apps/storybook-react-native/.storybook/FontLoader.js
+++ apps/storybook-react-native/.storybook/FontLoader.js
@@ -8,16 +8,16 @@ const FontLoader = ({ children }) => {
   useEffect(() => {
     (async () => {
       await Font.loadAsync({
-        'Geist Regular': require('../fonts/Geist/Geist Regular.otf'),
-        'Geist Regular Italic': require('../fonts/Geist/Geist Regular Italic.otf'),
-        'Geist Medium': require('../fonts/Geist/Geist Medium.otf'),
-        'Geist Medium Italic': require('../fonts/Geist/Geist Medium Italic.otf'),
-        'Geist Bold': require('../fonts/Geist/Geist Bold.otf'),
-        'Geist Bold Italic': require('../fonts/Geist/Geist Bold Italic.otf'),
-        'MM Poly Regular': require('../fonts/MMPoly/MM Poly Regular.otf'),
-        'MM Sans Regular': require('../fonts/MMSans/MM Sans Regular.otf'),
-        'MM Sans Medium': require('../fonts/MMSans/MM Sans Medium.otf'),
-        'MM Sans Bold': require('../fonts/MMSans/MM Sans Bold.otf'),
+        'Geist-Regular': require('../fonts/Geist/Geist-Regular.otf'),
+        'Geist-RegularItalic': require('../fonts/Geist/Geist-RegularItalic.otf'),
+        'Geist-Medium': require('../fonts/Geist/Geist-Medium.otf'),
+        'Geist-MediumItalic': require('../fonts/Geist/Geist-MediumItalic.otf'),
+        'Geist-Bold': require('../fonts/Geist/Geist-Bold.otf'),
+        'Geist-BoldItalic': require('../fonts/Geist/Geist-BoldItalic.otf'),
+        'MMPoly-Regular': require('../fonts/MMPoly/MMPoly-Regular.otf'),
+        'MMSans-Regular': require('../fonts/MMSans/MMSans-Regular.otf'),
+        'MMSans-Medium': require('../fonts/MMSans/MMSans-Medium.otf'),
+        'MMSans-Bold': require('../fonts/MMSans/MMSans-Bold.otf'),
       });
       setFontsLoaded(true);
     })();
