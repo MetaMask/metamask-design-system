2709ededca2686e5c1d8d00ecfbf0094eec8f144
kirillzyusko <zyusko.kirik@gmail.com>
2026-02-04 14:20:54 +0100

    feat: migrate `Overlay` component into react-native design system

---
+++ packages/design-system-react-native/src/components/Overlay/Overlay.stories.tsx
@@ -0,0 +1,28 @@
+// Third party dependencies.
+import React from 'react';
+
+// External dependencies.\
+import { BoxBackgroundColor } from '../../types';
+
+// Internal dependencies.
+import OverlayComponent from './Overlay';
+
+const OverlayMeta = {
+  title: 'Component Library / Overlay',
+  component: OverlayComponent,
+  argTypes: {
+    color: {
+      control: { type: 'color' },
+      defaultValue: BoxBackgroundColor.OverlayDefault,
+    },
+  },
+};
+export default OverlayMeta;
+
+export const Overlay = {
+  // TODO: Replace "any" with type
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  render: (args: any) => (
+    <OverlayComponent {...args} onPress={() => console.log("I'm clicked!")} />
+  ),
+};
---
+++ packages/design-system-react-native/src/components/Overlay/Overlay.types.ts
@@ -0,0 +1,20 @@
+// Third party dependencies.
+import { ColorValue, TouchableOpacityProps } from 'react-native';
+
+/**
+ * Overlay component props.
+ */
+export type OverlayProps = {
+  /**
+   * Color of the Overlay.
+   *
+   * @default theme.colors.overlay.default
+   */
+  color?: ColorValue;
+  /**
+   * Duration of the overlay animation.
+   *
+   * @default DEFAULT_OVERLAY_ANIMATION_DURATION
+   */
+  duration?: number;
+} & TouchableOpacityProps;
