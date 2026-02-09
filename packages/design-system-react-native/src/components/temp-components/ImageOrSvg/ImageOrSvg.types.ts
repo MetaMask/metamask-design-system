7ae7aab83c8e25798ad3769ba06fb9b311202c68
Brian Nguyen <brianacnguyen@gmail.com>
2026-02-09 15:45:51 -0800

    Removed unnecessary prop in dsrn

--- packages/design-system-react-native/src/components/temp-components/ImageOrSvg/ImageOrSvg.types.ts
+++ packages/design-system-react-native/src/components/temp-components/ImageOrSvg/ImageOrSvg.types.ts
@@ -41,10 +41,6 @@ export type ImageOrSvgBaseProps = {
    */
   style?: StyleProp<ImageStyle>;
   /**
-   * Optional prop to add twrnc overriding classNames.
-   */
-  twClassName?: string;
-  /**
    * Additional props to pass to the Image component, excluding
    * the 'source' prop (which is handled separately).
    */
