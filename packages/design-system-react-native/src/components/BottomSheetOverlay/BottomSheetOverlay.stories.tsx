4bc0d6b12b110bd1ebd9c2fe6014a99ac3391ec5
georgewrmarshall <george.marshall@consensys.net>
2026-02-11 21:57:15 -0800

    chore: updating story

--- packages/design-system-react-native/src/components/BottomSheetOverlay/BottomSheetOverlay.stories.tsx
+++ packages/design-system-react-native/src/components/BottomSheetOverlay/BottomSheetOverlay.stories.tsx
@@ -2,13 +2,12 @@ import type { Meta, StoryObj } from '@storybook/react-native';
 import React from 'react';
 
 import { Box } from '../Box';
-import { Text } from '../Text';
 
 import { BottomSheetOverlay } from './BottomSheetOverlay';
 import type { BottomSheetOverlayProps } from './BottomSheetOverlay.types';
 
 const meta: Meta<BottomSheetOverlayProps> = {
-  title: 'Components/BottomSheets/BottomSheetOverlay',
+  title: 'Components/BottomSheetOverlay',
   component: BottomSheetOverlay,
   argTypes: {
     onPress: { action: 'pressed' },
@@ -16,10 +15,7 @@ const meta: Meta<BottomSheetOverlayProps> = {
   },
   decorators: [
     (Story) => (
-      <Box twClassName="w-full h-96 relative">
-        <Box twClassName="absolute inset-0 justify-center items-center">
-          <Text>Content behind overlay</Text>
-        </Box>
+      <Box twClassName="w-full h-full relative">
         <Story />
       </Box>
     ),
@@ -29,15 +25,4 @@ const meta: Meta<BottomSheetOverlayProps> = {
 export default meta;
 type Story = StoryObj<BottomSheetOverlayProps>;
 
-export const Default: Story = {
-  args: {
-    onPress: undefined,
-  },
-};
-
-export const OnPress: Story = {
-  args: {
-    onPress: () => console.log('Overlay pressed'),
-  },
-  render: (args) => <BottomSheetOverlay {...args} />,
-};
+export const Default: Story = {};
