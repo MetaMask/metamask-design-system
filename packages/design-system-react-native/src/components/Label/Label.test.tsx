7247f955c5eea6cebed118ce8449a416e6934d60
kirillzyusko <zyusko.kirik@gmail.com>
2026-02-16 19:19:42 +0100

    fix: unit-tests

--- packages/design-system-react-native/src/components/Label/Label.test.tsx
+++ packages/design-system-react-native/src/components/Label/Label.test.tsx
@@ -2,7 +2,6 @@ import { render } from '@testing-library/react-native';
 import React from 'react';
 
 import { Label } from './Label';
-import { DEFAULT_LABEL_TEXT_VARIANT } from './Label.constants';
 
 describe('Label', () => {
   it('renders correctly', () => {
@@ -12,10 +11,8 @@ describe('Label', () => {
     expect(getByTestId('label')).toBeDefined();
   });
 
-  it('renders the given text with the default variant', () => {
+  it('renders children content', () => {
     const { getByText } = render(<Label>Sample Label Text</Label>);
-    const labelElement = getByText('Sample Label Text');
-    expect(labelElement).toBeDefined();
-    expect(labelElement.props.variant).toBe(DEFAULT_LABEL_TEXT_VARIANT);
+    expect(getByText('Sample Label Text')).toBeDefined();
   });
 });
