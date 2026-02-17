6964e7e3ccb0e79d48f0d6f7ab8b952b871102ee
georgewrmarshall <george.marshall@consensys.net>
2026-02-17 14:33:20 -0800

    chore: update

--- packages/design-system-shared/jest.config.js
+++ packages/design-system-shared/jest.config.js
@@ -15,7 +15,10 @@ module.exports = merge(baseConfig, {
   displayName,
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ['\\.types\\.ts$', '/types/.*/index\\.ts$'],
+  coveragePathIgnorePatterns: [
+    '\\.types\\.ts$', // ignore all .types.ts files
+    '/types/.*/index\\.ts$', // ignore index.ts files in types folder
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
