1233a9500ec8216e293198864779e556ffc41fb3
Brian Nguyen <brianacnguyen@gmail.com>
2026-02-16 17:27:40 -0800

    Added SafeAreaContext to jest setup

--- packages/design-system-react-native/jest.config.js
+++ packages/design-system-react-native/jest.config.js
@@ -43,7 +43,7 @@ module.exports = merge(baseConfig, {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!react-native|@react-native|react-native-reanimated|@react-navigation)',
+    'node_modules/(?!react-native|@react-native|react-native-reanimated|@react-navigation|react-native-safe-area-context)',
   ],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleNameMapper: {
