6d9356d90a9580fed35d014ee3dff18f4cffa871
georgewrmarshall <george.marshall@consensys.net>
2026-02-18 08:46:37 -0800

    test: format styles test file

--- packages/design-system-react-native/src/test-utils/styles.test.ts
+++ packages/design-system-react-native/src/test-utils/styles.test.ts
@@ -1,16 +1,14 @@
-import {
-  getResolvedStyle,
-  resolvedStyleIncludes,
-} from './styles';
+import { getResolvedStyle, resolvedStyleIncludes } from './styles';
 
 describe('test-utils/styles', () => {
   describe('getResolvedStyle', () => {
     it('returns merged style object for style arrays', () => {
-      expect(getResolvedStyle([{ opacity: 0.5 }, { backgroundColor: 'blue' }]))
-        .toStrictEqual({
-          opacity: 0.5,
-          backgroundColor: 'blue',
-        });
+      expect(
+        getResolvedStyle([{ opacity: 0.5 }, { backgroundColor: 'blue' }]),
+      ).toStrictEqual({
+        opacity: 0.5,
+        backgroundColor: 'blue',
+      });
     });
 
     it('returns an empty object when flattened style is not an object', () => {
@@ -21,10 +19,9 @@ describe('test-utils/styles', () => {
   describe('resolvedStyleIncludes', () => {
     it('returns true when resolved style contains expected style', () => {
       expect(
-        resolvedStyleIncludes(
-          [{ opacity: 1 }, { backgroundColor: 'black' }],
-          { backgroundColor: 'black' },
-        ),
+        resolvedStyleIncludes([{ opacity: 1 }, { backgroundColor: 'black' }], {
+          backgroundColor: 'black',
+        }),
       ).toBe(true);
     });
 
