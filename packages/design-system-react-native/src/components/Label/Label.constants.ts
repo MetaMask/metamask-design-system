b5f64eec0399f32fdda5538cddb8ad7ba3a0b4d6
kirillzyusko <zyusko.kirik@gmail.com>
2026-02-19 18:13:53 +0100

    fix: changes after review

--- packages/design-system-react-native/src/components/Label/Label.constants.ts
+++
@@ -1,4 +0,0 @@
-import { TextVariant } from '../Text';
-
-// Defaults
-export const DEFAULT_LABEL_TEXT_VARIANT = TextVariant.BodyMd;
--- packages/design-system-react-native/src/components/Label/Label.tsx
+++ packages/design-system-react-native/src/components/Label/Label.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
-import { Text } from '../Text';
+import { Text, TextVariant } from '../Text';
 
-import { DEFAULT_LABEL_TEXT_VARIANT } from './Label.constants';
 import type { LabelProps } from './Label.types';
 
 export const Label: React.FC<LabelProps> = ({ ...props }) => (
-  <Text variant={DEFAULT_LABEL_TEXT_VARIANT} {...props} />
+  <Text variant={TextVariant.BodyMd} {...props} />
 );
