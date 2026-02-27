932ab8c38cbd3b8d800bf269d99823b395793217
kirillzyusko <zyusko.kirik@gmail.com>
2026-02-27 20:04:55 +0100

    fix: re-work according to common guidelines

--- packages/design-system-react-native/src/components/ButtonSemantic/ButtonSemantic.tsx
+++ packages/design-system-react-native/src/components/ButtonSemantic/ButtonSemantic.tsx
@@ -1,68 +1,83 @@
 import React, { useCallback } from 'react';
 
-import { ButtonBase, ButtonSize } from '@metamask/design-system-react-native';
-import { useTailwind } from '@metamask/design-system-twrnc-preset';
+import { ButtonBase } from '../ButtonBase';
+import { IconSize } from '../Icon';
+import { TextVariant, FontWeight } from '../Text';
 
-// Internal dependencies.
 import {
-  ButtonSemanticProps,
-  ButtonSemanticSeverity,
-} from './ButtonSemantic.types';
+  TWCLASSMAP_BUTTONSEMANTIC_BG,
+  TWCLASSMAP_BUTTONSEMANTIC_BG_PRESSED,
+  TWCLASSMAP_BUTTONSEMANTIC_TEXT,
+} from './ButtonSemantic.constants';
+import type { ButtonSemanticProps } from './ButtonSemantic.types';
+import { ButtonSemanticSeverity } from './ButtonSemantic.types';
 
-const ButtonSemantic: React.FC<ButtonSemanticProps> = ({
+export const ButtonSemantic = ({
   severity,
-  size = ButtonSize.Lg,
+  children,
+  textProps,
+  spinnerProps,
+  startIconProps,
+  endIconProps,
+  isLoading = false,
+  twClassName = '',
+  style,
   ...props
-}) => {
-  const tw = useTailwind();
+}: ButtonSemanticProps) => {
+  const getContainerClassName = useCallback(
+    (pressed: boolean): string => {
+      const classNameStr =
+        typeof twClassName === 'function' ? twClassName(pressed) : twClassName;
 
-  const getTextClassName = useCallback(
-    (_pressed: boolean) => {
-      switch (severity) {
-        case ButtonSemanticSeverity.Success:
-          return 'text-success-default';
-        case ButtonSemanticSeverity.Danger:
-          return 'text-error-default';
-        default:
-          return 'text-success-default';
-      }
+      const bgClass =
+        pressed || isLoading
+          ? (TWCLASSMAP_BUTTONSEMANTIC_BG_PRESSED[severity] ??
+            TWCLASSMAP_BUTTONSEMANTIC_BG_PRESSED[
+              ButtonSemanticSeverity.Success
+            ])
+          : (TWCLASSMAP_BUTTONSEMANTIC_BG[severity] ??
+            TWCLASSMAP_BUTTONSEMANTIC_BG[ButtonSemanticSeverity.Success]);
+
+      return `${bgClass} ${classNameStr}`;
     },
-    [severity],
+    [severity, isLoading, twClassName],
   );
 
-  const getStyle = useCallback(
-    ({ pressed }: { pressed: boolean }) => {
-      let baseStyle: string;
-      let pressedStyle: string;
-
-      switch (severity) {
-        case ButtonSemanticSeverity.Success:
-          baseStyle = 'bg-success-muted';
-          pressedStyle = 'bg-success-muted-pressed';
-          break;
-        case ButtonSemanticSeverity.Danger:
-          baseStyle = 'bg-error-muted';
-          pressedStyle = 'bg-error-muted-pressed';
-          break;
-        default:
-          baseStyle = 'bg-success-muted';
-          pressedStyle = 'bg-success-muted-pressed';
-          break;
-      }
-
-      return [tw.style(baseStyle, pressed && pressedStyle), props.style];
-    },
-    [tw, severity, props.style],
+  const getTextClassName = useCallback(
+    (_pressed: boolean): string =>
+      TWCLASSMAP_BUTTONSEMANTIC_TEXT[severity] ??
+      TWCLASSMAP_BUTTONSEMANTIC_TEXT[ButtonSemanticSeverity.Success],
+    [severity],
   );
 
   return (
     <ButtonBase
-      size={size}
+      textProps={{
+        variant: TextVariant.BodyMd,
+        fontWeight: FontWeight.Medium,
+        numberOfLines: 1,
+        ellipsizeMode: 'clip',
+        ...textProps,
+      }}
+      spinnerProps={{
+        ...spinnerProps,
+      }}
+      startIconProps={{
+        size: IconSize.Sm,
+        ...startIconProps,
+      }}
+      endIconProps={{
+        size: IconSize.Sm,
+        ...endIconProps,
+      }}
+      isLoading={isLoading}
+      twClassName={getContainerClassName}
       textClassName={getTextClassName}
+      iconClassName={getTextClassName}
+      style={style}
       {...props}
-      style={getStyle}
-    />
+    >
+      {children}
+    </ButtonBase>
   );
 };
-
-export default ButtonSemantic;
