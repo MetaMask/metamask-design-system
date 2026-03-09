e548bc94f2f66daea334e8136d16a3edd44eef9f
georgewrmarshall <george.marshall@consensys.net>
2026-03-09 09:03:09 -0700

    Add missing react native component exports

--- packages/design-system-react-native/src/components/index.ts
+++ packages/design-system-react-native/src/components/index.ts
@@ -55,6 +55,19 @@ export type {
 export { BannerBase } from './BannerBase';
 export type { BannerBaseProps } from './BannerBase';
 
+export { BottomSheet } from './BottomSheet';
+export type {
+  BottomSheetProps,
+  BottomSheetRef,
+  BottomSheetPostCallback,
+} from './BottomSheet';
+
+export { default as BottomSheetDialog } from './BottomSheetDialog';
+export type {
+  BottomSheetDialogProps,
+  BottomSheetDialogRef,
+} from './BottomSheetDialog';
+
 export { BottomSheetFooter, ButtonsAlignment } from './BottomSheetFooter';
 export type {
   BottomSheetFooterProps,
@@ -82,7 +95,7 @@ export {
   BoxBackgroundColor,
   BoxBorderColor,
 } from './Box';
-export type { BoxProps, BoxSpacing } from './Box';
+export type { BoxProps, BoxSpacing, BoxBorderWidth } from './Box';
 
 export { Card } from './Card';
 export type { CardProps } from './Card';
@@ -92,6 +105,7 @@ export type { ButtonAnimatedProps } from './temp-components/ButtonAnimated';
 
 export { ButtonBase, ButtonBaseSize } from './ButtonBase';
 export type { ButtonBaseProps } from './ButtonBase';
+export { TWCLASSMAP_BUTTONBASE_SIZE_DIMENSION } from './ButtonBase';
 
 export { Button, ButtonSize, ButtonVariant } from './Button';
 export type { ButtonProps } from './Button';
@@ -135,6 +149,9 @@ export type { MainActionButtonProps } from './MainActionButton';
 export { Skeleton } from './Skeleton';
 export type { SkeletonProps } from './Skeleton';
 
+export { SensitiveText, SensitiveTextLength } from './SensitiveText';
+export type { SensitiveTextProps } from './SensitiveText';
+
 export { TabEmptyState } from './TabEmptyState';
 export type { TabEmptyStateProps } from './TabEmptyState';
 
@@ -157,9 +174,19 @@ export type { TextFieldProps } from './TextField';
 export { TextOrChildren } from './temp-components/TextOrChildren';
 export type { TextOrChildrenProps } from './temp-components/TextOrChildren';
 
-export { Toast, ToastVariant, ToastContext } from './Toast';
+export {
+  Toast,
+  ToastVariant,
+  ToastCloseButtonVariant,
+  ToastContext,
+  ToastContextWrapper,
+  TOAST_VISIBILITY_DURATION,
+  TOAST_ANIMATION_DURATION,
+  TOAST_BOTTOM_PADDING,
+} from './Toast';
 export type {
   ToastOptions,
+  ToastProps,
   ToastRef,
   ToastContextParams,
   ToastLabelOptions,
@@ -173,3 +200,12 @@ export type { ButtonHeroProps } from './ButtonHero';
 
 export { ButtonFilter } from './ButtonFilter';
 export type { ButtonFilterProps } from './ButtonFilter';
+
+export { ImageOrSvg } from './temp-components/ImageOrSvg';
+export type {
+  ImageOrSvgProps,
+  ImageOrSvgSrc,
+} from './temp-components/ImageOrSvg';
+
+export { Spinner } from './temp-components/Spinner';
+export type { SpinnerProps } from './temp-components/Spinner';
