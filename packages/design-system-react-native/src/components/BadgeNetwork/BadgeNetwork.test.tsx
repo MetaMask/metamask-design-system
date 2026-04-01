951ab000bef63bd96d4ded4a30e093b621f27d14
georgewrmarshall <george.marshall@consensys.net>
2026-03-31 20:21:44 -0700

    chore: updating badge network test

--- packages/design-system-react-native/src/components/BadgeNetwork/BadgeNetwork.test.tsx
+++ packages/design-system-react-native/src/components/BadgeNetwork/BadgeNetwork.test.tsx
@@ -1,28 +1,31 @@
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 import React from 'react';
-
-import { AvatarNetworkSize } from '../AvatarNetwork';
 
 import { BadgeNetwork } from './BadgeNetwork';
 
 const remoteImageSrc = { uri: 'https://example.com/photo.png' };
+
 describe('BadgeNetwork', () => {
-  it('renders an AvatarNetwork with size forced to Xs and forwards additional props', () => {
-    const { getByTestId } = render(
+  it('renders AvatarNetwork and shows fallback text on image error', () => {
+    const { getByTestId, getByText } = render(
       <BadgeNetwork
         src={remoteImageSrc}
         testID="badge-network"
-        imageOrSvgProps={{ imageProps: { testID: 'image-or-svg' } }}
+        fallbackText="ETH"
+        imageOrSvgProps={{
+          imageProps: { testID: 'image-or-svg' },
+        }}
       />,
     );
-    const renderedComponent = getByTestId('badge-network');
-    expect(renderedComponent).toBeDefined();
-    expect(renderedComponent.props.style[0].height.toString()).toStrictEqual(
-      (Number(AvatarNetworkSize.Xs) + 2).toString(),
-    );
 
-    expect(renderedComponent.props.children.props.src).toStrictEqual(
-      remoteImageSrc,
-    );
+    const badgeNetwork = getByTestId('badge-network');
+    const imageOrSvg = getByTestId('image-or-svg');
+
+    expect(badgeNetwork).toBeOnTheScreen();
+    expect(imageOrSvg).toBeOnTheScreen();
+
+    fireEvent(imageOrSvg, 'onImageError', { nativeEvent: { error: 'error' } });
+
+    expect(getByText('ETH')).toBeOnTheScreen();
   });
 });
