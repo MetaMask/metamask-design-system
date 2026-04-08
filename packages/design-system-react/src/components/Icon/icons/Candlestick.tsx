ed4192a3c7b3b1fb3ebe9cef82fe18671bae5a4a
georgewrmarshall <george.marshall@consensys.net>
2026-04-08 15:57:54 -0700

    fix: use double quotes in generated React icon TSX components
    Updates the SVGR template to emit double-quoted imports, matching the
    pre-existing convention in the icons/ folder and preventing spurious
    diffs on future regeneration runs.

--- packages/design-system-react/src/components/Icon/icons/Candlestick.tsx
+++ packages/design-system-react/src/components/Icon/icons/Candlestick.tsx
@@ -1,6 +1,6 @@
-import type { SVGProps, Ref } from 'react';
-import * as React from 'react';
-import { forwardRef } from 'react';
+import type { SVGProps, Ref } from "react";
+import * as React from "react";
+import { forwardRef } from "react";
 const SvgCandlestick = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" ref={ref} {...props}><path d="M9.5 5.331H7.831V7H6.165V17h1.667v1.668H9.5V17h1.668V7H9.499zm0 10.003H7.831V8.666H9.5zM17.835 8.666h-1.667V5.33H14.5v3.335h-1.667V14.5H14.5v4.168h1.667V14.5h1.667zm-1.667 4.168H14.5v-2.501h1.667z" /></svg>;
 const ForwardRef = forwardRef(SvgCandlestick);
 export default ForwardRef;
--- packages/design-system-react/src/components/Icon/icons/FirstPage.tsx
+++ packages/design-system-react/src/components/Icon/icons/FirstPage.tsx
@@ -1,6 +1,6 @@
-import type { SVGProps, Ref } from 'react';
-import * as React from 'react';
-import { forwardRef } from 'react';
+import type { SVGProps, Ref } from "react";
+import * as React from "react";
+import { forwardRef } from "react";
 const SvgFirstPage = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" ref={ref} {...props}><path d="M5.8 18V6h2v12zm11 0-6-6 6-6 1.4 1.4-4.6 4.6 4.6 4.6z" /></svg>;
 const ForwardRef = forwardRef(SvgFirstPage);
 export default ForwardRef;
