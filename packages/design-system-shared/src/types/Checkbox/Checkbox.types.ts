c71942693939f9850a8d5653cb879adbc4843516
georgewrmarshall <george.marshall@consensys.net>
2026-04-12 17:16:18 -0700

    chore: removing spaces

--- packages/design-system-shared/src/types/Checkbox/Checkbox.types.ts
+++ packages/design-system-shared/src/types/Checkbox/Checkbox.types.ts
@@ -13,33 +13,28 @@ export type CheckboxPropsShared = {
    * in your parent component.
    */
   isSelected: boolean;
-
   /**
    * Optional prop that when true, disables the checkbox.
    *
    * @default false
    */
   isDisabled?: boolean;
-
   /**
    * Optional prop that when true, displays the invalid/error state of the checkbox.
    *
    * @default false
    */
   isInvalid?: boolean;
-
   /**
    * Optional label prop that renders text or a React node as a label beside the checkbox.
    */
   label?: ReactNode | string;
-
   /**
    * Optional props to be passed to the label's Text component.
    * Supports shared text props (variant, color, fontWeight, fontFamily, fontStyle).
    * Platform packages extend this with platform-specific text props.
    */
   labelProps?: Omit<Partial<TextPropsShared>, 'children'>;
-
   /**
    * Required callback for when the checked state changes.
    * Use this to update your state.
