e6b83a1f58b8657f674e9e79869ecdbc40a9fb52
georgewrmarshall <george.marshall@consensys.net>
2026-04-17 14:25:42 -0700

    chore(storybook-rn): rely on Expo metro defaults

--- apps/storybook-react-native/metro.config.js
+++ apps/storybook-react-native/metro.config.js
@@ -1,26 +1,14 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withStorybook } = require('@storybook/react-native/metro/withStorybook');
-const path = require('path');
 
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, '../..');
-
-const defaultConfig = getDefaultConfig(projectRoot);
+const defaultConfig = getDefaultConfig(__dirname);
 
 // Add SVG transformer support
 defaultConfig.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
 
 // Update asset and source extensions for SVG support
 defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter((ext) => ext !== 'svg');
-defaultConfig.resolver.sourceExts = [...defaultConfig.resolver.sourceExts, 'svg', 'mjs'];
-
-// Configure for monorepo - watch workspace root and resolve from workspace node_modules
-defaultConfig.watchFolders = [workspaceRoot];
-defaultConfig.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(workspaceRoot, 'node_modules'),
-];
-
+defaultConfig.resolver.sourceExts = [...defaultConfig.resolver.sourceExts, 'svg'];
 
 module.exports = withStorybook(defaultConfig, {
   enabled: true,
