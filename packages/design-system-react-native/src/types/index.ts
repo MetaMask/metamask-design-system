252baee547d3fbd3d0956a515d221dfbf55701e7
georgewrmarshall <george.marshall@consensys.net>
2026-04-30 15:41:30 -0700

    refactor: remove AvatarGroup exports from RN type index

--- packages/design-system-react-native/src/types/index.ts
+++ packages/design-system-react-native/src/types/index.ts
@@ -6,16 +6,5 @@ export {
  * TODO: Remove the following exports and update imports in components to import directly from `@metamask/design-system-shared` once all components have been migrated to React Native.
  */
 export { IconColor, IconName, IconSize } from '@metamask/design-system-shared';
-export { AvatarBaseSize as AvatarGroupSize } from '@metamask/design-system-shared';
 export { AvatarBaseSize as AvatarNetworkSize } from '@metamask/design-system-shared';
 export { AvatarBaseSize as AvatarSize } from '@metamask/design-system-shared';
-
-/**
- * AvatarGroup - variant
- */
-export enum AvatarGroupVariant {
-  Account = 'account',
-  Favicon = 'favicon',
-  Network = 'network',
-  Token = 'token',
-}
