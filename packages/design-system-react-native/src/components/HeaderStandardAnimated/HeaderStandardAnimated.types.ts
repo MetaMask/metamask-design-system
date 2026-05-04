1fd7097c3151a7331cafaf8843b738fa6ee9a0d3
Brian Nguyen <brianacnguyen@gmail.com>
2026-05-04 15:12:51 -0700

    Updated test imports

--- packages/design-system-react-native/src/components/HeaderStandardAnimated/HeaderStandardAnimated.types.ts
+++ packages/design-system-react-native/src/components/HeaderStandardAnimated/HeaderStandardAnimated.types.ts
@@ -1,6 +1,8 @@
 // External dependencies.
-import type { SharedValue } from 'react-native-reanimated';
-import { useAnimatedScrollHandler } from 'react-native-reanimated';
+import type {
+  SharedValue,
+  useAnimatedScrollHandler,
+} from 'react-native-reanimated';
 
 // Internal dependencies.
 import type { HeaderStandardProps } from '../HeaderStandard/HeaderStandard.types';
