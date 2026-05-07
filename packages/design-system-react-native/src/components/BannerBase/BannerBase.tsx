ed334c9ea77987498a1ec866be6a28c20f19df80
georgewrmarshall <george.marshall@consensys.net>
2026-05-07 15:15:51 -0700

    refactor: rely on types for BannerBase close props

--- packages/design-system-react-native/src/components/BannerBase/BannerBase.tsx
+++ packages/design-system-react-native/src/components/BannerBase/BannerBase.tsx
@@ -43,7 +43,6 @@ export const BannerBase: React.FC<BannerBaseProps> = ({
 
   const {
     accessibilityLabel: closeButtonAccessibilityLabel = 'Close banner',
-    onPress: _ignoredCloseButtonOnPress,
     twClassName: closeButtonTwClassName,
     ...resolvedCloseButtonProps
   } = closeButtonProps ?? {};
