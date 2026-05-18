381fc06ff73023fe344e69ace67010ea745ee013
Brian Nguyen <brianacnguyen@gmail.com>
2026-05-18 08:38:57 -0700

    Updated telegram icon

--- packages/design-system-react/src/components/Icon/icons/Telegram.tsx
+++ packages/design-system-react/src/components/Icon/icons/Telegram.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import type { SVGProps } from "react";
 import { Ref, forwardRef } from "react";
-const SvgTelegram = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" ref={ref} {...props}><path d="M20.71 3.655s1.942-.758 1.78 1.082c-.053.757-.54 3.408-.917 6.276l-1.295 8.495s-.108 1.244-1.08 1.46c-.97.217-2.428-.757-2.698-.973-.215-.162-4.047-2.597-5.396-3.788-.378-.324-.81-.974.054-1.731l5.666-5.41c.648-.65 1.295-2.165-1.403-.325l-7.554 5.14s-.864.54-2.483.054l-3.507-1.082s-1.295-.812.917-1.624C8.19 8.686 14.828 6.09 20.71 3.654" /></svg>;
+const SvgTelegram = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" ref={ref} {...props}><path d="M20.295 3.813s1.85-.721 1.696 1.03c-.052.722-.514 3.247-.874 5.978l-1.233 8.09s-.103 1.186-1.028 1.392-2.313-.721-2.57-.928c-.205-.154-3.854-2.473-5.14-3.607-.359-.309-.77-.927.052-1.649l5.397-5.153c.616-.618 1.233-2.061-1.337-.309l-7.195 4.896s-.822.515-2.364.051l-3.34-1.03s-1.234-.774.873-1.547c5.14-2.422 11.461-4.895 17.063-7.214" /></svg>;
 const ForwardRef = forwardRef(SvgTelegram);
 export default ForwardRef;
