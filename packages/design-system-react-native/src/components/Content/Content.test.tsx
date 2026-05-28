3b6c5b11d826a35955a8eedc742e0bffca9e0ffc
Brian Nguyen <brianacnguyen@gmail.com>
2026-05-28 08:36:32 -0700

    Updated render hooks import

--- packages/design-system-react-native/src/components/Content/Content.test.tsx
+++ packages/design-system-react-native/src/components/Content/Content.test.tsx
@@ -5,8 +5,7 @@ import {
   TextVariant,
 } from '@metamask/design-system-shared';
 import { useTailwind } from '@metamask/design-system-twrnc-preset';
-import { renderHook } from '@testing-library/react-hooks';
-import { render } from '@testing-library/react-native';
+import { render, renderHook } from '@testing-library/react-native';
 import React from 'react';
 import { Text, View } from 'react-native';
 
