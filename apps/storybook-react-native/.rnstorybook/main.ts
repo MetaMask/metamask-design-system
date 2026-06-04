cfb270e7661fa0705ad2a280041c008f5fee0973
georgewrmarshall <george.marshall@consensys.net>
2026-06-04 14:38:18 -0700

    chore: drop storybook ondevice controls

--- apps/storybook-react-native/.rnstorybook/main.ts
+++ apps/storybook-react-native/.rnstorybook/main.ts
@@ -7,7 +7,6 @@ const config: StorybookConfig = {
   ],
   deviceAddons: [
     '@storybook/addon-ondevice-notes',
-    '@storybook/addon-ondevice-controls',
     '@storybook/addon-ondevice-backgrounds',
     '@storybook/addon-ondevice-actions',
   ],
