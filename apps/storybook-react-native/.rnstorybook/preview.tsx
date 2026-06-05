810a6fa528b62b550e01fd48bf11f6ae831161fd
georgewrmarshall <george.marshall@consensys.net>
2026-06-05 09:12:14 -0700

    chore: rollback

--- apps/storybook-react-native/.rnstorybook/preview.tsx
+++ apps/storybook-react-native/.rnstorybook/preview.tsx
@@ -1,26 +1,13 @@
 import type { Preview } from '@storybook/react-native';
 import { Theme, ThemeProvider } from '@metamask/design-system-twrnc-preset';
-import { darkTheme, lightTheme } from '@metamask/design-tokens';
 import React, { type PropsWithChildren } from 'react';
 import { useColorScheme } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const ThemeDecorator = ({ children }: PropsWithChildren) => {
   const colorScheme = useColorScheme();
   const theme = colorScheme === 'dark' ? Theme.Dark : Theme.Light;
-  const backgroundColor =
-    theme === Theme.Dark
-      ? darkTheme.colors.background.default
-      : lightTheme.colors.background.default;
 
-  return (
-    <GestureHandlerRootView style={{ flex: 1, backgroundColor }}>
-      <SafeAreaProvider>
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
-      </SafeAreaProvider>
-    </GestureHandlerRootView>
-  );
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 };
 
 const preview: Preview = {
