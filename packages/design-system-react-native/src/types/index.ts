2991789b32d1c633474a7a11fefc4a745ae6d298
georgewrmarshall <george.marshall@consensys.net>
2026-06-08 14:00:54 -0700

    Remove local icon type re-exports

--- packages/design-system-react-native/src/types/index.ts
+++ packages/design-system-react-native/src/types/index.ts
@@ -3,8 +3,7 @@ export {
   AvatarBaseShape,
 } from '@metamask/design-system-shared';
 /**
- * TODO: Remove the following exports and update imports in components to import directly from `@metamask/design-system-shared` once all components have been migrated to React Native.
+ * TODO: Remove the following aliases and update imports in components to import directly from `@metamask/design-system-shared` once all components have been migrated to React Native.
  */
-export { IconColor, IconName, IconSize } from '@metamask/design-system-shared';
 export { AvatarBaseSize as AvatarNetworkSize } from '@metamask/design-system-shared';
 export { AvatarBaseSize as AvatarSize } from '@metamask/design-system-shared';
