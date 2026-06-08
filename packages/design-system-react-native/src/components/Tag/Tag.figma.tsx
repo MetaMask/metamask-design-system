0315debd04b6cbee15b40a9dc75c43901643e501
Amanda Yeoh <147617420+amandaye0h@users.noreply.github.com>
2026-06-08 23:13:28 +0800

    chore: Update error > danger for severity prop, render stories with Box instead of Div

--- packages/design-system-react-native/src/components/Tag/Tag.figma.tsx
+++ packages/design-system-react-native/src/components/Tag/Tag.figma.tsx
@@ -28,7 +28,7 @@ figma.connect(
     props: {
       severity: figma.enum('severity', {
         neutral: TagSeverity.Neutral,
-        error: TagSeverity.Error,
+        error: TagSeverity.Danger,
         info: TagSeverity.Info,
         success: TagSeverity.Success,
         warning: TagSeverity.Warning,
