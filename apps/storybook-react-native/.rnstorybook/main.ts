2632c0bd22c776fc0f82f27e72956b19a5a05c6e
georgewrmarshall <george.marshall@consensys.net>
2026-06-09 17:00:16 -0700

    fix: re-add ondevice-controls to RN storybook deviceAddons

--- apps/storybook-react-native/.rnstorybook/main.ts
+++ apps/storybook-react-native/.rnstorybook/main.ts
@@ -7,6 +7,7 @@ const config: StorybookConfig = {
   ],
   deviceAddons: [
     '@storybook/addon-ondevice-notes',
+    '@storybook/addon-ondevice-controls',
     '@storybook/addon-ondevice-backgrounds',
     '@storybook/addon-ondevice-actions',
   ],
